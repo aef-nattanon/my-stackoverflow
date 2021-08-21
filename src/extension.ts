3c7cfc54d6ad5ab640c645ff5ab759d70546edd9
aef-nattanon <48983118+aef-nattanon@users.noreply.github.com>
2021-08-22 01:20:30 +0700

    Remove webview.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,14 +1,12 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import {getHtmlContent} from './webview';
+import * as path from 'path';
+import * as fs from 'fs';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	let stackOverflowView = vscode.commands.registerCommand('my-stackoverflow.myView',() => {
-		// Gotta play with this API :)
-		// https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=relevance&q=get+date+in+javascript&answers=1&site=stackoverflow
-		    // Create and show panel
 		const panel = vscode.window.createWebviewPanel(
 			'stackOverflow',
 			'My StackOverFlow 6430621001 Demo',
@@ -16,10 +14,9 @@ export function activate(context: vscode.ExtensionContext) {
 			{
 				enableScripts: true
 			}
-		);	
-	
-		// And set its HTML content
-		panel.webview.html = getHtmlContent();
+		);
+		const filePath: vscode.Uri = vscode.Uri.file(path.join(context.extensionPath, 'ui', 'index.html'));
+		panel.webview.html = fs.readFileSync(filePath.fsPath,'utf8');
 	});
 	context.subscriptions.push(stackOverflowView);
 }
