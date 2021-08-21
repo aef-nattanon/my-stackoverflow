f4ac4a28d3fca61b98e52670d1582d4239c4f85a
aef-nattanon <48983118+aef-nattanon@users.noreply.github.com>
2021-08-21 20:59:52 +0700

    init version 1

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,27 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
+import {getHtmlContent} from './webview';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	let stackOverflowView = vscode.commands.registerCommand('my-stackoverflow.myView',() => {
+		// Gotta play with this API :)
+		// https://api.stackexchange.com/2.2/search/advanced?order=desc&sort=relevance&q=get+date+in+javascript&answers=1&site=stackoverflow
+		    // Create and show panel
+		const panel = vscode.window.createWebviewPanel(
+			'stackOverflow',
+			'My StackOverFlow 6430621001',
+			vscode.ViewColumn.Two,
+			{
+				enableScripts: true
+			}
+		);	
 	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "my-stackoverflow" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('my-stackoverflow.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from my-stackoverflow!');
+		// And set its HTML content
+		panel.webview.html = getHtmlContent();
 	});
-
-	context.subscriptions.push(disposable);
+	context.subscriptions.push(stackOverflowView);
 }
 
 // this method is called when your extension is deactivated
---
+++ src/webview.ts
@@ -0,0 +1,40 @@
+// I've only included HTML in this and CSS and JS files are linked externally
+// If you are here to contribute to the UI just edit the files in /ui folder and test it from index.html and then create a pull request
+// I will take the files from ui folder and will host it over cdn
+const html = `
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Integrated Stackoverflow Search</title>
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://thephukettrip.com/style2.css">
+        <!-- <link rel="stylesheet" href="https://raw.githubusercontent.com/aef-nattanon/vscode-integrated-stackoverflow/master/ui/style1.css"> -->
+        <!-- <link rel="stylesheet" href="file:///Users/nattanon/Codes/tesis/vscode-integrated-stackoverflow/ui/style.css"> -->
+        <link rel="shortcut icon" href="logo-192.png" type="image/x-icon">
+    </head>
+    <body>
+        <div style="padding:20px;">
+            <!-- <label for="search" style="color:#09f;">Search</label> -->
+            <div style="display:flex;">
+                <input id="search" style="display:inline-block;width:75%;padding:6px;flex:1;" type="search">
+                <button class="search-button blue-button">Search</button>
+            </div>
+            <div id="loading" style="display: none;">Loading...</div>
+            <div id="answers" style="margin-top:20px;">
+
+            </div>
+        </div>
+        <script src="https://thephukettrip.com/script1.js"></script>
+        <!-- <script src="https://raw.githubusercontent.com/aef-nattanon/vscode-integrated-stackoverflow/master/ui/script.js"></script> -->
+        <!-- <script src="file:///Users/nattanon/Codes/tesis/vscode-integrated-stackoverflow/ui/script.js"></script> -->
+    </body>
+</html>
+`;
+
+
+export const getHtmlContent=()=> {
+    console.log(html);
+    return html;
+};
